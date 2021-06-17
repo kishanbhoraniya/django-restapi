8097cc0e4476c808c8387db34641ba5c6e499eba
Kishan Bhoraniya <kishanbhoraniya2@gmail.com>
2021-06-17 09:40:52 +0530

    a

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,8 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://databasebackend.h
 #     }
 # }
 
+DATABASES = { 'default': dj_database_url.config() }
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
@@ -65,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.runserver_nostatic',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -131,14 +133,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-#location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'')
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
